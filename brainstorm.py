b20539dd73be98294b91b468b40ee2a08be486a1
yunweneric <yunweneric@mail.com>
2020-07-31 14:02:08 +0100

    edited python script

--- brainstorm.py
+++ brainstorm.py
@@ -1,3 +1,2 @@
-# extract pdf for the first time
-# write initial results as pdf
-# Read second pdf again and extract content using regex
+while True:
+    print("hello world")
--- realcopy.py
+++ realcopy.py
@@ -174,7 +174,7 @@ def Get_Oldata(gce):
 
         #1.3. splice out name and store in a variable
         # 4.3: Extracting raw data
-        Raw_data = Raw_data + "" + page.extractText()
+        Raw_data = Raw_data + " " + page.extractText()
         # 4.4: Replacing bannar
     Data = Raw_data.replace("2019 RESULTS: GCE ADVANCED LEVEL GENERAL", "")
     Data = Data.replace("2019 RESULTS: GCE ORDINARY LEVEL GENERAL", "")
@@ -184,6 +184,99 @@ def Get_Oldata(gce):
     import re
     Data = Data.replace("\n", "")
     Data = re.sub(r"\d+.\d+of\d+\w+", "", Data)
+    # Data = re.sub(r"\(\d+\)", "", Data)
+    Data = re.sub(r"Passed In \d+ Subjects: \d+", "", Data)
+    Data = re.sub(r"Passed In \d+ Subjects:\d+", "", Data)
+    Data = re.sub(r"\( In \d+ Subjects: \d+", "", Data)
+    # Data = Data.replace("(", "")
+
+
+
+    Data = Data.replace("HIS-A", "HIS-A ")
+    Data = Data.replace("HIS-B", "HIS-B ")
+    Data = Data.replace("HIS-C", "HIS-C ")
+
+    Data = Data.replace("GEO-A", "GEO-A ")
+    Data = Data.replace("GEO-B", "GEO-B ")
+    Data = Data.replace("GEO-C", "GEO-C ")
+
+    Data = Data.replace("MAT-A", "MAT-A ")
+    Data = Data.replace("MAT-B", "MAT-B ")
+    Data = Data.replace("MAT-C", "MAT-C ")
+
+    Data = Data.replace("CHE-A", "CHE-A ")
+    Data = Data.replace("CHE-B", "CHE-B ")
+    Data = Data.replace("CHE-C", "CHE-C ")
+
+    Data = Data.replace("BIO-A", "BIO-A ")
+    Data = Data.replace("BIO-B", "BIO-B ")
+    Data = Data.replace("BIO-C", "BIO-C ")
+
+
+    Data = Data.replace("ECO-A", "ECO-A ")
+    Data = Data.replace("ECO-B", "ECO-B ")
+    Data = Data.replace("ECO-C", "ECO-C ")
+
+    Data = Data.replace("PHY-A", "PHY-A ")
+    Data = Data.replace("PHY-B", "PHY-B ")
+    Data = Data.replace("PHY-C", "PHY-C ")
+
+    Data = Data.replace("REL-A", "REL-A ")
+    Data = Data.replace("REL-B", "REL-B ")
+    Data = Data.replace("REL-C", "REL-C ")
+
+    Data = Data.replace("LOG-A", "LOG-A ")
+    Data = Data.replace("LOG-B", "LOG-B ")
+    Data = Data.replace("LOG-C", "LOG-C ")
+
+
+    Data = Data.replace("COM-A", "COM-A ")
+    Data = Data.replace("COM-B", "COM-B ")
+    Data = Data.replace("COM-C", "COM-C ")
+
+    Data = Data.replace("LIT-A", "LIT-A ")
+    Data = Data.replace("LIT-B", "LIT-B ")
+    Data = Data.replace("LIT-C", "LIT-C ")
+
+
+    Data = Data.replace("HBI-A", "HBI-A ")
+    Data = Data.replace("HBI-B", "HBI-B ")
+    Data = Data.replace("HBI-C", "HBI-C ")
+
+
+    Data = Data.replace("CZE-A", "CZE-A ")
+    Data = Data.replace("CZE-B", "CZE-B ")
+    Data = Data.replace("CZE-C", "CZE-C ")
+
+    Data = Data.replace("ENG-A", "ENG-A ")
+    Data = Data.replace("ENG-B", "ENG-B ")
+    Data = Data.replace("ENG-C", "ENG-C ")
+
+    Data = Data.replace("FRE-A", "FRE-A ")
+    Data = Data.replace("FRE-B", "FRE-B ")
+    Data = Data.replace("FRE-C", "FRE-C ")
+
+    Data = Data.replace("AMA-A", "AMA-A ")
+    Data = Data.replace("AMA-B", "AMA-B ")
+    Data = Data.replace("AMA-C", "AMA-C ")
+
+    Data = Data.replace("SBEF-A","SBEF-A ")
+    Data = Data.replace("SBEF-B","SBEF-B ")
+    Data = Data.replace("SBEF-C","SBEF-C ")
+    print(Data)
+
+
+
+
+
+
+
+
+
+
+
+
+
     Data = re.sub(r"Sanctioned : \d+", "\n", Data)
     Data = Data.replace("Centre No:  ", '\n\n\n\n"Centre No": ')
     Data = Data.replace("Regist:", '\nRegist:')
@@ -194,95 +287,21 @@ def Get_Oldata(gce):
     # Data = Data.replace("Sanctioned : ", '\nSanctioned: \n')
 
     # Replacing the Passed in x subjects section
-    Data = Data.replace("Passed In 11 Subjects: ", '\nPassed In 11 Subjects:')
-    Data = Data.replace("Passed In 10 Subjects: ", ' \n\nPassed In 10 Subjects: ')
-    Data = Data.replace("Passed In 9 Subjects: ", '\nPassed In 9 Subjects:')
-    Data = Data.replace("Passed In 8 Subjects: ", ' \n\nPassed In 8 Subjects: ')
-    Data = Data.replace("Passed In 7 Subjects: ", '\nPassed In 7 Subjects:')
-    Data = Data.replace("Passed In 6 Subjects: ", ' \n\nPassed In 6 Subjects: ')
-    Data = Data.replace("Passed In 5 Subjects: ", '\nPassed In 5 Subjects:')
-    Data = Data.replace("Passed In 4 Subjects: ", ' \n\nPassed In 4 Subjects: ')
-    Data = re.sub(r"Passed In \d+ Subjects: \d+", "", Data)
-    Data = re.sub(r"Passed In \d+ Subjects:\d+", "", Data)
-    Data = re.sub(r"\( In \d+ Subjects: \d+", "", Data)
+    # Data = Data.replace("Passed In 11 Subjects: ", '\nPassed In 11 Subjects:')
+    # Data = Data.replace("Passed In 10 Subjects: ", ' \n\nPassed In 10 Subjects: ')
+    # Data = Data.replace("Passed In 9 Subjects: ", '\nPassed In 9 Subjects:')
+    # Data = Data.replace("Passed In 8 Subjects: ", ' \n\nPassed In 8 Subjects: ')
+    # Data = Data.replace("Passed In 7 Subjects: ", '\nPassed In 7 Subjects:')
+    # Data = Data.replace("Passed In 6 Subjects: ", ' \n\nPassed In 6 Subjects: ')
+    # Data = Data.replace("Passed In 5 Subjects: ", '\nPassed In 5 Subjects:')
+    # Data = Data.replace("Passed In 4 Subjects: ", ' \n\nPassed In 4 Subjects: ')
+    
     # Data = Data.replace("( In 9 Subjects: 16", "")
 
     Data = Data.replace("(", " (")
     
     # USE REGEX TO MODIFY THE STUDENT'S RESULTS. EX. HIS-BNAME ===> HIS-B NAME............
-   
-    # Data = Data.replace("HIS-A", "HIS-A")
-    # Data = Data.replace("HIS-B", "HIS-B")
-    # Data = Data.replace("HIS-C", "HIS-C")
-
-    # Data = Data.replace("GEO-A", "GEO-A")
-    # Data = Data.replace("GEO-B", "GEO-B")
-    # Data = Data.replace("GEO-C", "GEO-C")
-
-    
-    # Data = Data.replace("MAT-A", "MAT-A")
-    # Data = Data.replace("MAT-B", "MAT-B")
-    # Data = Data.replace("MAT-C", "MAT-C")
-
-    # Data = Data.replace("CHE-A", "CHE-A")
-    # Data = Data.replace("CHE-B", "CHE-A")
-    # Data = Data.replace("CHE-C", "CHE-A")
-
-    # Data = Data.replace("BIO-A", "BIO-A")
-    # Data = Data.replace("BIO-B", "BIO-B")
-    # Data = Data.replace("BIO-C", "BIO-C")
-
-
-    # Data = Data.replace("ECO-A", "ECO-A")
-    # Data = Data.replace("ECO-B", "ECO-B")
-    # Data = Data.replace("ECO-C", "ECO-C")
-
-    # Data = Data.replace("PHY-A", "PHY-A")
-    # Data = Data.replace("PHY-B", "PHY-B")
-    # Data = Data.replace("PHY-C", "PHY-C")
-
-    # Data = Data.replace("REL-A", "REL-A")
-    # Data = Data.replace("REL-B", "REL-B")
-    # Data = Data.replace("REL-C", "REL-C")
-
-    # Data = Data.replace("LOG-A", "LOG-A")
-    # Data = Data.replace("LOG-B", "LOG-B")
-    # Data = Data.replace("LOG-C", "LOG-C")
-
-
-    # Data = Data.replace("COM-A", "COM-A")
-    # Data = Data.replace("COM-B", "COM-B")
-    # Data = Data.replace("COM-C", "COM-C")
-
-    # Data = Data.replace("LIT-A", "LIT-A")
-    # Data = Data.replace("LIT-B", "LIT-B")
-    # Data = Data.replace("LIT-C", "LIT-C")
-
-
-    # Data = Data.replace("HBI-A", "HBI-A")
-    # Data = Data.replace("HBI-B", "HBI-B")
-    # Data = Data.replace("HBI-C", "HBI-C")
-
-
-    # Data = Data.replace("CZE-A", "CZE-A")
-    # Data = Data.replace("CZE-B", "CZE-B")
-    # Data = Data.replace("CZE-C", "CZE-C")
-
-    # Data = Data.replace("ENG-A", "ENG-A")
-    # Data = Data.replace("ENG-B", "ENG-B")
-    # Data = Data.replace("ENG-C", "ENG-C")
-
-    # Data = Data.replace("FRE-A", "FRE-A")
-    # Data = Data.replace("FRE-B", "FRE-B")
-    # Data = Data.replace("FRE-C", "FRE-C")
-
-    # Data = Data.replace("AMA-A", "AMA-A")
-    # Data = Data.replace("AMA-B", "AMA-B")
-    # Data = Data.replace("AMA-C", "AMA-C")
-
-    # Data = Data.replace("SBEF-A","SBEF-A")
-    # Data = Data.replace("SBEF-B","SBEF-B")
-    # Data = Data.replace("SBEF-C","SBEF-C")
+  
 
 
     print(Data)
@@ -393,9 +412,3 @@ def Get_Oldata(gce):
 Get_Oldata("./pdfs/2019-olgen.pdf")
 #=======================================================================================================================
 # /home/yunwen/Documents/Seven Advanced/Projects/GCE-platform-project/pdfs/2019-algen.pdf
-
-
-
-
-
-
